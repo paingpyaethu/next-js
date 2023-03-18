0d4d4e85dd5f49f681c5999e82ad09f65c55bfa6
paingpyaethu <paingpyaethu@gmail.com>
2023-03-18 15:24:15 +0630

    Fetching Data in Parallel

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -4,8 +4,20 @@ type Params = {
   };
 };
 
+import getUser from "@/lib/getUser";
+import getUserPosts from "@/lib/getUserPosts";
 import React from "react";
 
-export default function UserPage({ params: { userId } }: Params) {
-  return <></>;
+export default async function UserPage({ params: { userId } }: Params) {
+  const userData: Promise<User> = getUser(userId);
+  const userPostsData: Promise<Post[]> = getUserPosts(userId);
+
+  const [user, userPosts] = await Promise.all([userData, userPostsData]);
+  return (
+    <>
+      <h2>{user.name}</h2>
+      <br />
+      <UserPosts posts={userPosts} />
+    </>
+  );
 }
