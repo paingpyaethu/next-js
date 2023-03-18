e33f94279602e4e44c6ba74de78ae88de7ae1feb
paingpyaethu <paingpyaethu@gmail.com>
2023-03-18 13:00:20 +0630

    about nested layout, link, adding css module

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <h1>About</h1>
+      <Link href={"/"}>Link to Home Page</Link>
+    </>
+  );
 }
