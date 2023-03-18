9b16abba7778db2e2c8bbeeb4d60998ce2bc9fdf
paingpyaethu <paingpyaethu@gmail.com>
2023-03-18 15:09:59 +0630

    fetch users

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Hello World!</h1>
+      <h1>Home Page</h1>
       <Link href={"/about"}>Go to about page</Link>
+      <Link href={"/users"}>Users</Link>
     </main>
   );
 }
